e3601f41849bc9a857ed9fded0ebfe07aac02545
Isso-W <letian1726393174@gmail.com>
2023-05-01 02:53:02 +0200

    Javadocs

---
+++ minesweeper/src/MineSweeperPanel.java
@@ -0,0 +1,36 @@
+public class MineSweeperPanel {
+    /**
+     * Start a new game.
+     *
+     * @param  numRows  number of rows
+     * @param  numCols  number of columns
+     * @param  numMines  number of Mines
+     */
+    public void reset(int numRows, int numCols, int numMines){
+
+    }
+
+    /**
+     * check the grid:
+     *      if mine, game over
+     *      if not mine:
+     *          if mines nearby, show amount of mines
+     *          if no mines nearby, check all grid around with no mines nearby
+     *
+     * @param  x  position of the grid
+     * @param  y  position of the grid
+     */
+    public void check(int x, int y){
+
+    }
+
+    /**
+     * if right-click on a not checked grid, flag the grid as mine
+     * @param  x  position of the grid
+     * @param  y  position of the grid
+     */
+    private void flag(int x, int y){
+
+    }
+
+}
---
+++ minesweeper/src/MineSweeperTile.java
@@ -0,0 +1,16 @@
+public class MineSweeperTile {
+    /**
+     *  set the grid as mine
+     */
+    public void setMine(){
+
+    }
+    /**
+     *  check the grid
+     * @return   true if it is mine, false if not.
+     */
+    public boolean isMine(){
+
+        return false;
+    }
+}
