920075f05fa2a3c6e8b9546504b6551e0f9ab83e
Isso-W <letian1726393174@gmail.com>
2023-06-17 22:50:52 +0200

    method getSurroundPosition added

--- minesweeper/src/main/java/com/example/minesweeper/MineSweeperPanel.java
+++ minesweeper/src/main/java/com/example/minesweeper/MineSweeperPanel.java
@@ -1,26 +1,30 @@
 package com.example.minesweeper;
 import java.util.*;
 public class MineSweeperPanel {
+    int length;
+    int width;
+    int numMines;
+    public MineSweeperPanel(int length, int width, int numMines){
+        this.length = length;
+        this.width = width;
+        this.numMines = numMines;
+    }
     public MineSweeperTile[][] board;
     /**
      * Start a new game.
-     *
-     * @param  x  number of columns
-     * @param  x  number of rows
-     * @param  numMines  number of Mines
      */
-    public void reset(int x, int y, int numMines){
+    public void reset(){
         //create panel
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
                 board[i][j] = new MineSweeperTile();
             }
         }
 
         //set mines
         Random random = new Random();
-        int xValue = random.nextInt(x);
-        int yValue = random.nextInt(y);
+        int xValue = random.nextInt(length);
+        int yValue = random.nextInt(width);
         while (numMines != 0){
             if (!board[xValue][yValue].isMine()) {
                 board[xValue][yValue].setMine();
@@ -44,6 +48,32 @@ public class MineSweeperPanel {
             return;
         }
         //TODO
+    }
+    /**
+     * get all position near the given position
+     *
+     * @param  x  position of the grid
+     * @param  y  position of the grid
+     * @return a list of all positions near the given position
+     */
+    public List<MineSweeperTile> getSurroundPosition(int x, int y){
+        int xMin = x--;
+        int xMax = x++;
+        int yMin = y--;
+        int yMax = y++;
+
+        if (x == 0){xMin = 0;}
+        if (y == 0){yMin = 0;}
+        if (x == width){xMax = width;}
+        if (y == length){yMax = length;}
+
+        List<MineSweeperTile> result = new ArrayList<>();
+        for (int i = xMin; i < xMax; i++) {
+            for (int j = yMin; j < yMax; j++) {
+                result.add(board[y][x]);
+            }
+        }
+        return result;
     }
 
     /**
